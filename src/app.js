4a71d8c7b7f9b2797cd5a0214d488d9f569ffc61
Fabio Ribeiro <si.fabioribeiro@gmail.com>
2020-04-07 00:01:08 -0300

    Created methods get, post and put

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,15 +11,55 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  const { title } = request.params
+
+  response.status(200).json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const { title, url, techs } = request.body
+  const repositore = {
+    id: uuid(),
+    title,
+    url: `http://github.com/${url}`,
+    techs,
+    likes: 0
+  };
+
+  console.log(repositore);
+
+  repositories.push(repositore);
+
+  response.json(repositore);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoreIndex = repositories.findIndex(repositore => repositore.id === id);
+
+  if (repositoreIndex < 0) {
+    return response.status(400).json({
+      error: 'Repositore not found'
+    })
+  }
+  
+  const repositore = {
+    id,
+    title,
+    url,
+    techs
+  }
+
+  repositories[repositoreIndex] = repositore;
+
+  return response.json(repositore);
+
 });
 
 app.delete("/repositories/:id", (req, res) => {
